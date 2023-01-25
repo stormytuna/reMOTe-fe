ecc20332c6d06a0b06cc85250c34628b5bbd8ac7
K-Othman <karim_othman88@hotmail.com>
2023-01-25 17:06:03 +0000

    implement vueJS setup

---
+++ remote-fe/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+  <div>
+    <Header title="reMOTe services" />
+  </div>
+
+  <!-- <router-view /> -->
+</template>
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+/* Start Global Rules */
+* {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Open Sans", sans-serif;
+  font-family: "Poppins", sans-serif;
+}
+html {
+  scroll-behavior: smooth;
+}
+a {
+  text-decoration: none;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.container {
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: auto;
+  margin-right: auto;
+}
+/* Small */
+@media (min-width: 768px) {
+  .container {
+    width: 750px;
+  }
+}
+/* Medium */
+@media (min-width: 992px) {
+  .container {
+    width: 970px;
+  }
+}
+/* Large */
+@media (min-width: 1200px) {
+  .container {
+    width: 1170px;
+  }
+}
+/* End Global Rules */
+</style>
