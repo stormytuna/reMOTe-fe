74e61cee8ed2e00268059526a1b6213f8f994bf5
Sana <sanagubari27@gmail.com>
2023-01-30 15:18:31 +0000

    Create single technician page

--- remote-fe/src/components/SingleTechnician.vue
+++ remote-fe/src/components/SingleTechnician.vue
@@ -1,10 +1,139 @@
 <template>
-  <h1>hello</h1>
+  <main>
+    <img
+      :style="{
+        width: computedWidth,
+        height: '144px',
+        objectFit: 'cover',
+      }"
+      :src="card.avatarUrl"
+      class="card-img-top img-fluid"
+      :alt="card.firstName"
+    />
+
+    <h2 class="emphasise container">
+      {{ `${card.firstName} ${card.lastName}` }}
+    </h2>
+
+    <div class="card container service-container" style="width: 20rem">
+      <div class="card-body">
+        <div class="title-and-button">
+          <div>
+            <h5 class="card-title">MOT</h5>
+
+            <p class="card-text">Price: £20</p>
+          </div>
+          <a href="#" class="btn btn-danger">Add to Basket</a>
+        </div>
+      </div>
+      <v-divider inset></v-divider>
+      <div class="card-body">
+        <div class="title-and-button">
+          <div>
+            <h5 class="card-title">MOT</h5>
+
+            <p class="card-text">Price: £20</p>
+          </div>
+          <a href="#" class="btn btn-danger">Add to Basket</a>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
   name: "SingleTechnician",
+  data() {
+    return {
+      windowWidth: 0,
+      card: [],
+      technicianData: [],
+    };
+  },
+  mounted() {
+    fetch(
+      "https://remote-be.onrender.com/api/technicians/63ce75449ae462be0adad72d"
+    )
+      .then((res) => res.json())
+
+      .then((data) => {
+        this.card = data.technician;
+        this.technicianData = data.technician.technician;
+      })
+      .catch((err) => console.log(err.message));
+
+    this.windowWidth = window.innerWidth;
+    window.addEventListener("resize", this.handleResize);
+  },
+  methods: {
+    averageRatings(reviewsArray) {
+      console.log(reviewsArray, "<<<<<<,reviews");
+      let total = 0;
+      reviewsArray.forEach((review) => {
+        total += review.rating;
+      });
+      return total / reviewsArray.length;
+    },
+  },
+  computed: {
+    computedWidth() {
+      if (this.windowWidth > 765) {
+        return "240px";
+      } else {
+        return this.windowWidth + "px";
+      }
+    },
+  },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.emphasise {
+  color: #f8333c;
+  font-weight: 700;
+  font-size: 1.75rem;
+  line-height: 1.29;
+
+  margin: 20px;
+}
+.reviews-number {
+  font-weight: 600;
+  margin-bottom: 0px;
+  font-size: 0.8rem;
+  padding-right: 15px;
+  color: rgb(116, 116, 116);
+}
+.fa-star {
+  color: orange;
+  font-size: 0.9rem;
+}
+.rating {
+  font-size: 0.7rem;
+  margin-bottom: 0px;
+  padding-right: 15px;
+}
+
+.service-container {
+  border-radius: 10px;
+  line-height: 1.5;
+  color: #242e30;
+}
+.card-text {
+  font-size: 0.9rem;
+}
+.card-title {
+  font-weight: 700;
+  font-size: 1rem;
+}
+.title-and-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+a.btn.btn-danger {
+  font-size: 0.7rem;
+  font-weight: 500;
+  margin-bottom: var(--bs-card-title-spacer-y);
+}
+</style>
--- remote-fe/src/components/TechnicianNavBar.vue
+++ remote-fe/src/components/TechnicianNavBar.vue
@@ -1,39 +1,58 @@
 <template>
   <div class="dropdown">
-    <a
-      class="btn btn-secondary dropdown-toggle"
-      href="#"
-      role="button"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Sort By
-    </a>
+    <div class="dropdown-container">
+      <a
+        class="btn btn-secondary dropdown-toggle"
+        href="#"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Sort By
+      </a>
 
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Price</a></li>
-      <li><a class="dropdown-item" href="#">Reviews</a></li>
-      <li><a class="dropdown-item" href="#">Distance</a></li>
-    </ul>
-    <a
-      class="btn btn-secondary dropdown-toggle"
-      href="#"
-      role="button"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Order
-    </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Price</a></li>
+        <li><a class="dropdown-item" href="#">Reviews</a></li>
+        <li><a class="dropdown-item" href="#">Distance</a></li>
+      </ul>
+      <a
+        class="btn btn-secondary dropdown-toggle"
+        href="#"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Order
+      </a>
 
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Asc</a></li>
-      <li><a class="dropdown-item" href="#">Desc</a></li>
-    </ul>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Asc</a></li>
+        <li><a class="dropdown-item" href="#">Desc</a></li>
+      </ul>
+    </div>
+
+    <div class="container-fluid search-bar">
+      <form class="d-flex" role="search">
+        <input
+          class="form-control me-2 search"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "TechnicianNavBar",
+  computed: {
+    screenWidth() {
+      return window.innerWidth;
+    },
+  },
 };
 </script>
 <style scoped>
@@ -42,8 +61,7 @@ export default {
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  margin-bottom:30px;
-
+  margin-bottom: 30px;
 }
 
 .dropdown-toggle {
@@ -56,5 +74,20 @@ export default {
   box-shadow: 0px 2px 20px #e7e7e7;
   margin: 10px;
   font-weight: 600;
+}
+@media (max-width: 776px) {
+  .dropdown {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+  .search-bar {
+    margin-top: 15px;
+  }
+}
+.search {
+  border-radius: 10px;
+  font-size: 0.9rem;
 }
 </style>
